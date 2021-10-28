1c8e8692cb768f034fd94fd5db1069445db8d1ab
Sahil Bhatia <sahilofficial671@gmail.com>
2021-10-28 12:29:15 +0530

    Edited

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -33,7 +33,7 @@
 <div aria-labelledby="modal-title" role="dialog" aria-modal="true" {!! $attributes->merge(['class' => 'fixed z-50 inset-0 overflow-y-auto']) !!}>
     <div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block">
 
-        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+        <div class="fixed insert-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
 
         <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
 
